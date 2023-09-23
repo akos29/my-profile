302cd0b3107823faaa30cac9f12f2144cb945ec7
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-09-23 10:25:22 +0200

    updated  links are visible without needing to click 'see project'

--- src/pages/portfolio/ProjectModal.js
+++ src/pages/portfolio/ProjectModal.js
@@ -1,51 +1,40 @@
-import React, { useState } from 'react';
-import { Button, Modal } from 'react-bootstrap';
-import { AiFillGithub } from 'react-icons/ai';
-import { RxExternalLink } from 'react-icons/rx';
-import { BsBoxArrowUpRight }  from 'react-icons/bs';
+import React from "react";
+import { Modal } from "react-bootstrap";
+import { AiFillGithub } from "react-icons/ai";
+import { RxExternalLink } from "react-icons/rx";
+import { BsBoxArrowUpRight } from "react-icons/bs";
 
-function ProjectModal({ project,img }) {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+function ProjectModal({ project, img, show, handleClose }) {
+  if (!project) return null;
 
   return (
-    <>
-      <Button className='ac_btn btn' onClick={handleShow}>
-       See Project <BsBoxArrowUpRight />
-      </Button>
-      {/* <div className='bg__menu h-100'> */}
-      <Modal show={show} onHide={handleClose} className='po_modal'>
-        <Modal.Header closeButton className='po_header'>
-          <Modal.Title>{project.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className='menu_container'>
-          <ul className='po_skills'>
-            {project.tech?.map((skill, i) => {
-              return <li key={i}>{skill.name}</li>;
-            })}
-          </ul>
-          <div className='po_body'>
-          {/* <img src={img} alt={project.title} /> */}
+    <Modal show={show} onHide={handleClose} className="po_modal">
+      <Modal.Header closeButton className="po_header">
+        <Modal.Title>{project.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="menu_container">
+        <ul className="po_skills">
+          {project.tech?.map((skill, i) => {
+            return <li key={i}>{skill.name}</li>;
+          })}
+        </ul>
+        <div className="po_body">
           <div
-                  className="img "
-                  style={{ backgroundImage: `url(${img})` }}
-                ></div> 
-          <p className='text_2'>{project.description}</p>
-          </div>
-        <div className='po_footer'>
-          <a className='ac_btn btn' href={project.link} target="_blank">
+            className="img "
+            style={{ backgroundImage: `url(${img})` }}
+          ></div>
+          <p className="text_2">{project.description}</p>
+        </div>
+        <div className="po_footer">
+          <a className="ac_btn btn" href={project.link} target="_blank">
             See Live <RxExternalLink />
           </a>
-          <a className='ac_btn btn'  href={project.github} target="_blank">
+          <a className="ac_btn btn" href={project.github} target="_blank">
             See Source <AiFillGithub />
           </a>
         </div>
-        </Modal.Body>
-      </Modal>
-      {/* </div> */}
-    </>
+      </Modal.Body>
+    </Modal>
   );
 }
 
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Container, Row, Col } from "react-bootstrap";
 import { dataportfolio, meta } from "../../content_option";
-import ProjectModal from "./ProjectModal";
 import myImage1 from "../../assets/images/portfolio/1.jpg";
 import myImage2 from "../../assets/images/portfolio/2.jpg";
 import myImage3 from "../../assets/images/portfolio/3.jpg";
 import myImage4 from "../../assets/images/portfolio/4.jpg";
-import Kibrewossen_Mekasha_Resume from "../../assets/Kibrewossen_Mekasha_Resume.pdf"
-import { FiDownload } from 'react-icons/fi'
+import Kibrewossen_Mekasha_Resume from "../../assets/Kibrewossen_Mekasha_Resume.pdf";
+import { FiDownload } from "react-icons/fi";
+import { AiFillGithub } from "react-icons/ai";
+import { RxExternalLink } from "react-icons/rx";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+import ProjectModal from "./ProjectModal";
 
 export const Portfolio = () => {
   const myImages = [myImage1, myImage2, myImage3, myImage4];
+
+  // State to manage the modal
+  const [showModal, setShowModal] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  // Function to open the modal with selected project
+  const openModal = (project) => {
+    setSelectedProject(project);
+    setShowModal(true);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setSelectedProject(null);
+    setShowModal(false);
+  };
 
   return (
     <HelmetProvider>
@@ -27,29 +46,58 @@ export const Portfolio = () => {
             <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
-        </Row> 
-        <a href={Kibrewossen_Mekasha_Resume}  className="download" download="Kibrewossen_Mekasha_Resume.pdf" > Résumé PDF <FiDownload /></a> 
+        </Row>
+        <a
+          href={Kibrewossen_Mekasha_Resume}
+          className="download"
+          download="Kibrewossen_Mekasha_Resume.pdf"
+        >
+          {" "}
+          Résumé PDF <FiDownload />{" "}
+        </a>
         <div className="mb-3 po_items_ho">
-       
           {dataportfolio.map((data, i) => {
             return (
-              <div key={i} className="po_item card">
+              <div
+                key={i}
+                className="po_item card"
+                onClick={() => openModal(data)}
+              >
                 <div
                   className="img"
                   style={{ backgroundImage: `url(${myImages[data.img]})` }}
                 ></div>
-                {/* <img src={`${myImages[data.img]}`} alt={data.title} /> */}
                 <div class="card-body content">
                   <h5 class="card-title">{data.title}</h5>
-                  <p>{data.description.slice(0,150)}</p>
-                  {/* <a href={data.link}>view project</a> */}  
-                  <ProjectModal project={data} img={myImages[data.img]} />
+                  <p>{data.description.slice(0, 150)}</p>
+                  <div className="po_footer">
+                    <a className="ac_btn btn" href={data.link} target="_blank">
+                      See Live <RxExternalLink />
+                    </a>
+                    <a
+                      className="ac_btn btn"
+                      href={data.github}
+                      target="_blank"
+                    >
+                      See Source <AiFillGithub />
+                    </a>
+                  </div>
                 </div>
               </div>
             );
           })}
         </div>
       </Container>
+
+      {/* Project Modal */}
+      {selectedProject && (
+        <ProjectModal
+          project={selectedProject}
+          img={myImages[selectedProject.img]}
+          show={showModal}
+          handleClose={closeModal}
+        />
+      )}
     </HelmetProvider>
   );
 };
